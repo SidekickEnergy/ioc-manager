79d3217496e866b7033a4e84284f25a4d7668b13
Mikkel Christoffer S Schmidt <lgb668@ku.dk>
2025-09-16 14:47:44 +0200

    Modified startupsh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+# Activate Azure's virtual environment if it exists
+if [ -e "$HOME/antenv/bin/activate" ]; then
+  echo "[INFO] Activating virtual environment..."
+  source "$HOME/antenv/bin/activate"
+fi
+
+# Install (if needed) and start Gunicorn
 pip install -r requirements.txt
+
 export PYTHONPATH=./backend
 exec gunicorn app.api:app --chdir backend --bind=0.0.0.0:8000 --timeout 300

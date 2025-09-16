3b6c61978b8918713b6bcaac8601e246292b140a
Mikkel Christoffer S Schmidt <lgb668@ku.dk>
2025-09-16 14:16:08 +0200

    Modified startupsh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 pip install -r requirements.txt
-exec gunicorn backend.app.api:app --bind=0.0.0.0:8000 --timeout 300
-
+export PYTHONPATH=./backend
+exec gunicorn app.api:app --chdir backend --bind=0.0.0.0:8000 --timeout 300
